510d51849541faf917e6267136221cb0110ec657
wrnlb666 <wrnlb666@gmail.com>
2025-12-07 16:50:23 +1100

    forgot to add bashrc.local into bashrc

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env sh
 
 ln -sf $(pwd)/bashrc.local ~/.bashrc.local
+if ! grep -Fq '# my bash config' ~/.bashrc; then
+    echo "# my bash config" >> ~/.bashrc
+    echo "if [[ -r ~/.bashrc.local ]]; then" >> ~/.bashrc
+    echo "    source ~/.bashrc.local" >> ~/.bashrc
+    echo "fi" >> ~/.bashrc
+fi
