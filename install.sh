d1b2e089e113367885ceb49b1d0a89d5708f8fd4
wrnlb666 <wrnlb666@gmail.com>
2026-07-01 04:28:06 +1000

    add safe guard if file not exist

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
-ln -sf $(pwd)/bashrc.local ~/.bashrc.local
+ln -sf "$(pwd)/bashrc.local" ~/.bashrc.local
+[[ -f ~/.bashrc ]] || touch ~/.bashrc
 if ! grep -Fq '# my bash config' ~/.bashrc; then
     echo "# my bash config" >> ~/.bashrc
     echo "if [[ -r ~/.bashrc.local ]]; then" >> ~/.bashrc
