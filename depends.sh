87dd3d80f8fe43b5bc33fd9faf7fb46beb514cac
wrnlb666 <wrnlb666@gmail.com>
2026-09-13 03:16:41 +1000

    update dependency file for bash.conf

---
+++ depends.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+depends=(
+    'mise'
+    'starship'
+    'lsd'
+    'lazygit'
+    'lazydocker'
+    'uv'
+    'gomi'
+    'trash-put'
+    'bat'
+    'fzf'
+    'nvim'
+    'micro'
+    'edit'
+    'zoxide'
+    'fastfetch'
+    'spf'
+    'tmux'
+)
